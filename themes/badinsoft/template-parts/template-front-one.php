2a849553a79208c34116879f4bc7a1ae671875cd
Aleksandar Dojcinovic <aleksandar.dojcinovic@tranxactor.com>
2018-06-05 12:17:08 +0200

    Front Page contact block. Plugin - Snippets.

--- themes/badinsoft/template-parts/template-front-one.php
+++ themes/badinsoft/template-parts/template-front-one.php
@@ -12,10 +12,10 @@ get_header(); ?>
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
-			<?php while ( have_posts() ) : the_post(); ?>
+              <?php while ( have_posts() ) : the_post(); ?>
                 <article id="post-<?php the_ID(); ?>">
                     <section class="front-slider">
-						<?php echo do_shortcode( get_field( 'slider_shortcode' ) ); ?>
+                        <?php echo do_shortcode( get_field( 'slider_shortcode' ) ); ?>
                     </section>
                     <section class="trusted-companies">
                         <div class="container">
@@ -50,6 +50,7 @@ get_header(); ?>
                             </div>
                         </div>
                     </section>
+
                     <section class="services-section svg-animation">
                         <div class="container">
                             <div class="row">
@@ -154,6 +155,7 @@ get_header(); ?>
                             </div>
                         </div>
                     </section>
+
                     <section class="industry-expertise">
                         <div class="container">
                             <div class="row">
@@ -182,6 +184,7 @@ get_header(); ?>
                             </div>
                         </div>
                     </section>
+
                     <section class="testimonials-section">
                         <div class="container">
                             <div class="row">
@@ -259,6 +262,7 @@ get_header(); ?>
                             </div>
                         </div>
                     </section>
+
                     <section class="ceo-section">
                         <div class="container">
                             <div class="row">
@@ -305,22 +309,9 @@ get_header(); ?>
                             </div>
                         </div>
                     </section>
-                    <section class="blue-section">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-xs-12">
-                                    <h2>Contact Us</h2>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
-                                    <h3>We'd love to hear from you!</h3>
-                                    <p>Contact us with a brief description or specification of your project and we will contact you shortly to learn more about your needs.</p>
-									<?php echo do_shortcode( '[wpforms id="55" title="false" description="false"]' ) ?>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+
+                    <?php the_content(); ?>
+
                 </article>
 			<?php endwhile; // End of the loop. ?>
         </main><!-- #main -->
